35b7a26850cec0d64a5ff20d87bd740186a6a755
eduardoslg <eduardo.seve@hotmail.com>
2023-07-30 14:47:59 -0300

    typescript fix

--- src/_layouts/DefaultLayout/index.tsx
+++ src/_layouts/DefaultLayout/index.tsx
@@ -1,7 +1,5 @@
 import { Outlet } from 'react-router-dom'
 
-import { Flex } from '@siakit/layout'
-
 import { Foooter } from '../../components/Footer'
 import { Header } from '../../components/Header'
 import { LayoutContainer } from './styles'
